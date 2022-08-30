c5f90578c4113b9e3e9a324223a868a4ef14c28a
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-08-29 23:59:01 -0300

    feat: welcome

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
+
+
+class TesteController extends Controller
+{
+    public function index()
+    {
+        return (['Seja bem vindo ao LC 01']);
+    }
+}
