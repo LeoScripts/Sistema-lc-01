c317253e2c843fd319a0d4465d73664be130bec7
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-08-30 23:02:47 -0300

    refact: modify name method in UserController create to store

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,19 +20,6 @@ class UserController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create(Request $request)
-    {
-        $data = $request->all();
-        $data['password'] = bcrypt($request->password);
-        User::create($data);
-        return (['Usuario cadastrado!']);
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -40,7 +27,10 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        User::create($data);
+        return (['Usuario cadastrado!']);
     }
 
     /**
