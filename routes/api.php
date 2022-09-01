3ce90296258338d9e146ea5793e2df0e484fbbfe
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-09-01 02:31:48 -0300

    feat: route and method show user

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', [WelcomeController::class, 'index']);
 
+Route::get('/users/{id}', [UserController::class, 'show']);
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/users/create', [UserController::class, 'store']);
 
